8aa89b33fe3b85c8fdc3a01c82906665ca62e2b0
BJRambo <qw5414@naver.com>
2020-04-17 21:40:55 +0900

    class repack

--- lotterylotto.class.php
+++ lotterylotto.class.php
@@ -2,14 +2,67 @@
 
 class lotterylotto extends ModuleObject
 {
+	/**
+	 * 등록할 트리거를 여기에 선언하면 자동으로 등록된다.
+	 * checkUpdate(), moduleUpdate() 등에서 체크 및 생성 루틴을 중복으로 작성하지 않아도 된다.
+	 */
+	protected static $_insert_triggers = array(
+		// array('document.insertDocument', 'after', 'controller', 'triggerAfterInsertDocument'),
+		// array('document.updateDocument', 'after', 'controller', 'triggerAfterUpdateDocument'),
+		// array('document.deleteDocument', 'after', 'controller', 'triggerAfterDeleteDocument'),
+	);
 
-	/*****************************************
-	 * @brief 설치시 추가 작업이 필요할시 구현
-	 ******************************************/
+	/**
+	 * 이전 버전에서 등록했던 트리거를 삭제하려면 위와 동일한 문법으로 여기에 선언하면 된다.
+	 * 사용하지 않는 트리거는 삭제해 주는 것이 성능에 도움이 된다.
+	 */
+	protected static $_delete_triggers = array(
+		// array('comment.insertComment', 'after', 'controller', 'triggerAfterInsertComment'),
+		// array('comment.updateComment', 'after', 'controller', 'triggerAfterUpdateComment'),
+		// array('comment.deleteComment', 'after', 'controller', 'triggerAfterDeleteComment'),
+	);
+
+	// =========================== 이 부분 아래는 수정하지 않아도 된다 ============================
+
+	/**
+	 * 모듈 설정 캐시를 위한 변수.
+	 */
+	protected static $_config_cache = null;
+
+	public function getConfig()
+	{
+		if (self::$_config_cache === null)
+		{
+			$oModuleModel = getModel('module');
+			self::$_config_cache = $oModuleModel->getModuleConfig($this->module) ?: new stdClass;
+		}
+		return self::$_config_cache;
+	}
+
+	/**
+	 * 모듈 설정을 저장하는 함수.
+	 *
+	 * 설정을 변경할 필요가 있을 때 ModuleController를 직접 호출하지 말고 이 함수를 사용한다.
+	 * getConfig()으로 가져온 설정을 적절히 변경하여 setConfig()으로 다시 저장하는 것이 정석.
+	 *
+	 * @param object $config
+	 * @return object
+	 */
+	public function setConfig($config)
+	{
+		$oModuleController = getController('module');
+		$result = $oModuleController->insertModuleConfig($this->module, $config);
+		if ($result->toBool())
+		{
+			self::$_config_cache = $config;
+		}
+		return $result;
+	}
+	
 	function moduleInstall()
 	{
-		$oModuleModel = &getModel('module');
-		$oModuleController = &getController('module');
+		$oModuleModel = getModel('module');
+		$oModuleController = getController('module');
 		$module_info = $oModuleModel->getModuleInfoByMid('lotterylotto');
 		if (!$module_info->module_srl)
 		{
@@ -44,8 +97,8 @@ class lotterylotto extends ModuleObject
 	 ****************************************************/
 	function moduleUpdate()
 	{
-		$oModuleModel = &getModel('module');
-		$oModuleController = &getController('module');
+		$oModuleModel = getModel('module');
+		$oModuleController = getController('module');
 		//트리거 설치 (회원탈퇴 혹은 삭제시 해당회원의 로또복권 로그삭제)
 		if (!$oModuleModel->getTrigger('member.deleteMember', 'lotterylotto', 'controller', 'triggerAfterDeleteMember', 'after'))
 		{
